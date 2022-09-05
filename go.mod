565b20c8be889ce31438ce29dc505a6c20ae00f8
tenkoh <tenkoh.go@gmail.com>
2022-09-06 08:42:05 +0900

    wip: refactoring preprocess and collect-word

--- go.mod
+++ go.mod
@@ -3,13 +3,9 @@ module github.com/tenkoh/go-jpclouds
 go 1.18
 
 require (
-	github.com/fogleman/gg v1.3.0 // indirect
-	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
-	github.com/ikawaha/kagome-dict v1.0.5 // indirect
-	github.com/ikawaha/kagome-dict/ipa v1.0.6 // indirect
-	github.com/ikawaha/kagome-dict/uni v1.1.5 // indirect
-	github.com/ikawaha/kagome/v2 v2.8.2 // indirect
-	github.com/satori/go.uuid v1.2.0 // indirect
-	github.com/tenkoh/go-wordclouds v0.0.0-20220902103740-393914789e0a // indirect
-	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
+	github.com/ikawaha/kagome-dict/ipa v1.0.6
+	github.com/ikawaha/kagome/v2 v2.8.2
+	golang.org/x/text v0.3.7
 )
+
+require github.com/ikawaha/kagome-dict v1.0.5 // indirect
--- jpclouds.go
+++ jpclouds.go
@@ -1,33 +1,78 @@
 package jpclouds
 
 import (
+	"fmt"
 	"io"
+	"strings"
+
+	"golang.org/x/text/unicode/norm"
+	"golang.org/x/text/width"
 
 	"github.com/ikawaha/kagome-dict/ipa"
 	"github.com/ikawaha/kagome/v2/tokenizer"
 )
 
-func Nouns(r io.Reader) ([]string, error) {
+type SpeechPart int
+
+func (sp SpeechPart) String() string {
+	return [...]string{"名詞", "動詞", "形容詞"}[sp]
+}
+
+const (
+	Noun SpeechPart = iota
+	Verb
+	Adjective
+)
+
+func Preprocess(r io.Reader) (string, error) {
+	// unicode的表記ゆれを除去する
+	r = norm.NFKC.Reader(r)
+
+	b, err := io.ReadAll(r)
+	if err != nil {
+		return "", fmt.Errorf("error occured in preprocessing")
+	}
+
+	// 英数字は半角、カタカナは全角で統一する
+	b = width.Fold.Bytes(b)
+
+	s := string(b)
+
+	// 英数字を大文字で統一する
+	s = strings.ToUpper(s)
+
+	// remove spaces to detect multi-words english word as a one-word.
+	s = strings.ReplaceAll(s, " ", "")
+
+	return s, nil
+}
+
+func CollectWords(s string, targets []SpeechPart) ([]string, error) {
 	t, err := tokenizer.New(ipa.Dict(), tokenizer.OmitBosEos())
 	if err != nil {
 		return nil, err
 	}
-	b, err := io.ReadAll(r)
-	if err != nil {
-		return nil, err
+	var words []string
+	targetMap := map[string]struct{}{}
+	for _, target := range targets {
+		targetMap[target.String()] = struct{}{}
 	}
-	var nouns []string
-	tokens := t.Tokenize(string(b))
+	tokens := t.Tokenize(s)
 	for _, token := range tokens {
-		w, exist := token.FeatureAt(0)
+		sp, exist := token.FeatureAt(0)
 		if !exist {
 			continue
 		}
-		if w == "名詞" {
-			nouns = append(nouns, token.Surface)
+		if _, exist = targetMap[sp]; exist {
+			base, exist := token.BaseForm()
+			if exist {
+				words = append(words, base)
+			} else {
+				words = append(words, token.Surface)
+			}
 		}
 	}
-	return nouns, nil
+	return words, nil
 }
 
 func WordCount(words []string) (map[string]int, error) {
